74793451bce186604551875db5c46e546650a422
JA-Martina <martinaj@sheridancollege.ca>
2020-12-31 12:54:42 -0500

    BlackOps FAF EXUnits
    This is the original version accessible from the FAF mod vault. The next commit will modify this mod to bring one unit within into a state of compatibility with LOUD.

---
+++ BlackOpsFAF-EXUnits/mod_info.lua
@@ -0,0 +1,18 @@
+name = "BlackOps FAF: EXUnits"
+uid = "9e8ee941-c406-4751-b367-e10000019000"
+version = 19
+copyright = "2009 Exavier Macbeth"
+description = "The ultimate unit pack, re-engineered for FAF 3680 or later"
+author = "Exavier Macbeth, lt_hawkeye, orangeknight"
+icon = "/mods/BlackOpsFAF-EXUnits/icon.png"
+selectable = true
+enabled = true
+exclusive = false
+ui_only = false
+special_thanks = "IceDreamer, Uveso, Softles, Nojoke185, ZephyrWarrior, Raistlfiren, Nocaps, Netjaxx"
+requires = {}
+requiresNames = {}
+conflicts = {
+}
+before = {}
+after = {}
