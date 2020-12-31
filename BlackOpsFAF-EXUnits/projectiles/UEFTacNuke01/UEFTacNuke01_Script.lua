f4fc48b3f59763faae1084fd633748c297c694a9
JA-Martina <martinaj@sheridancollege.ca>
2020-12-31 12:59:15 -0500

    BLOPS-EX: Fix Seawolf tactical nuke
    This projectile relied on FAF methods which have now been either partially integrated or replaced.
    
    I should note that this unit is most of the reason I rolled this pack into LOUD Extras, though there's other cool stuff in EXUnits. I really like indirect fire submarines, it would seem.

--- BlackOpsFAF-EXUnits/projectiles/UEFTacNuke01/UEFTacNuke01_Script.lua
+++ BlackOpsFAF-EXUnits/projectiles/UEFTacNuke01/UEFTacNuke01_Script.lua
@@ -1,7 +1,12 @@
 --
 -- Terran CDR Nuke
 --
+
+local NullShell = import('/lua/sim/defaultprojectiles.lua').NullShell
 local TIFMissileNuke = import('/lua/terranprojectiles.lua').TIFMissileNuke
+local VDist2 = VDist2
+local ForkThread = ForkThread
+local WaitSeconds = WaitSeconds
 
 UEFTacNuke01 = Class(TIFMissileNuke) {
 
@@ -13,10 +18,47 @@ UEFTacNuke01 = Class(TIFMissileNuke) {
     },
     ThrustEffects = {'/effects/emitters/nuke_munition_launch_trail_04_emit.bp',},
 
+    LauncherCallbacks = function(self)
+		local launcher = self:GetLauncher()
+        if launcher and not launcher.Dead and launcher.EventCallbacks.ProjectileDamaged then
+            self.ProjectileDamaged = {}
+            for k,v in launcher.EventCallbacks.ProjectileDamaged do
+                table.insert(self.ProjectileDamaged, v)
+            end
+        end
+        self:SetCollisionShape('Sphere', 0, 0, 0, 2.0)
+        self:ForkThread(self.MovementThread)	
+	end,
+
+	DoTakeDamage = function(self, instigator, amount, vector, damageType)
+        if self.ProjectileDamaged then
+            for k,v in self.ProjectileDamaged do
+                v(self)
+            end
+        end
+        NullShell.DoTakeDamage(self, instigator, amount, vector, damageType)
+    end,
+    
+    OnDamage = function(self, instigator, amount, vector, damageType)
+		local bp = self:GetBlueprint().Defense.MaxHealth
+			if bp then
+			self:DoTakeDamage(instigator, amount, vector, damageType)
+		else
+			self:OnKilled(instigator, damageType)
+		end
+    end,
+
     OnCreate = function(self)
         TIFMissileNuke.OnCreate(self)
         self.effectEntityPath = '/mods/BlackOpsFAF-EXUnits/effects/Entities/EXETacNukeEffectController01/EXETacNukeEffectController01_proj.bp'
         self:LauncherCallbacks()
+    end,
+
+	CreateEffects = function(self, EffectTable, army, scale)
+        if not EffectTable then return end
+        for k, v in EffectTable do
+            self.Trash:Add(CreateAttachedEmitter(self, -1, army, v):ScaleEmitter(scale))
+        end
     end,
 
     -- Tactical nuke has different flight path
@@ -34,6 +76,13 @@ UEFTacNuke01 = Class(TIFMissileNuke) {
             self:SetTurnRateByDist()
             WaitSeconds(self.WaitTime)
         end
+    end,
+
+	GetDistanceToTarget = function(self)
+        local tpos = self:GetCurrentTargetPosition()
+        local mpos = self:GetPosition()
+        local dist = VDist2(mpos[1], mpos[3], tpos[1], tpos[3])
+        return dist
     end,
 
     SetTurnRateByDist = function(self)
@@ -56,6 +105,29 @@ UEFTacNuke01 = Class(TIFMissileNuke) {
             self:SetTurnRate(200)
             KillThread(self.MoveThread)
         end
+    end,
+
+	ForceThread = function(self)
+        -- Knockdown force rings
+        local position = self:GetPosition()
+        DamageRing(self, position, 0.1, 45, 1, 'Force', true)
+        WaitSeconds(0.1)
+        DamageRing(self, position, 0.1, 45, 1, 'Force', true)
+    end,
+	
+	OnImpact = function(self, TargetType, TargetEntity)
+        if not TargetEntity or not EntityCategoryContains(categories.PROJECTILE, TargetEntity) then
+            -- Play the explosion sound
+            local myBlueprint = self:GetBlueprint()
+            if myBlueprint.Audio.NukeExplosion then
+                self:PlaySound(myBlueprint.Audio.NukeExplosion)
+            end
+
+            nukeProjectile = self:CreateProjectile(self.effectEntityPath, 0, 0, 0, nil, nil, nil):SetCollision(false)
+            nukeProjectile:PassDamageData(self.DamageData)
+            nukeProjectile:PassData(self.Data)
+        end
+        NullShell.OnImpact(self, TargetType, TargetEntity)
     end,
 
     OnEnterWater = function(self)
