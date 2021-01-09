a2c18e453c3757b6ee4e9f1627219544377bbc76
TheRatCircus <bloxbuilder@gmail.com>
2021-01-09 14:23:18 -0500

    RatUnits: AI custom unit support

---
+++ RatUnits/lua/CustomUnits/Rat_CustomUnits.lua
@@ -0,0 +1,32 @@
+-- Rat_CustomUnits.lua
+-- Author: Rat Circus
+
+UnitList = {
+	-- I know this seems like nonsense but the Ennui isn't supposed
+	-- to be used like a Yathsou or Leviathan
+	T3SeaBattleship = {
+		Cybran = {'gmrs302', 35},
+	},
+
+	T3SeaNukeSub = {
+		Aeon = {'exuas0304', 50},
+		UEF = {'tbu0031', 50},
+		Cybran = {'gmrs401', 50},
+	},
+
+	T4LandExperimental2 = {
+		UEF = {'gmel414', 35}, -- Steroid
+	},
+
+	T4AirExperimental1 = {
+        UEF = {'brnat3bomber',100}, -- Havoc  
+    },
+
+	T4Artillery = {
+		-- MIRVs
+		Aeon = {'gmab407', 50},
+		UEF = {'gmeb420', 50},
+		Cybran = {'urb23051', 50},
+		Seraphim = {'gsmb319', 50},
+	},
+}
