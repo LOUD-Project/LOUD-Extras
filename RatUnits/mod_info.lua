55c674261aa8f6e14fd1b16f18bf9ed7de05442a
TheRatCircus <bloxbuilder@gmail.com>
2021-02-12 13:28:19 -0500

    RatUnits: Better UID, new icon, comment requires

--- RatUnits/mod_info.lua
+++ RatUnits/mod_info.lua
@@ -1,5 +1,5 @@
 name = "Rat's Unit Pack"
-uid = "55E98770-34A7-11EB-968E-RAT0FC2CA371"
+uid = "55E98770-34A7-11EB-968E-RAT0000UNITS"
 version = 0.1
 description = "Units scavenged from other mods. Maybe some custom ones one day. Balance not guaranteed. Not for use with anything but the LOUD mod."
 author = "Various Authors"
@@ -9,6 +9,7 @@ enabled = true
 exclusive = false
 ui_only = false
 requires = {
+	-- Total Mayhem
 	"62e2j64a-53a2-y6sg-32h5-146as555a18u3",
 }
 requiresNames = {}
