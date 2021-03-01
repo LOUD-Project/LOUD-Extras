02b21f96a9b5b97ce405c40ba8e16f242755c40c
TheRatCircus <bloxbuilder@gmail.com>
2021-03-01 12:12:46 -0500

    BLOPS-EX: Remove FAF labelling from frontend, directory
    Additionally, fix whatever file paths still led to files from BlackOps FAF instead of BlackOps LOUD.

--- BlackOpsFAF-EXUnits/projectiles/UEFHVM01/UEFHVM01_script.lua
+++
@@ -1,9 +0,0 @@
---
--- Terran Anti Air Missile
---
-local UEFHVM01Projectile = import('/mods/BlackOpsFAF-EXUnits/lua/EXBlackOpsprojectiles.lua').UEFHVM01Projectile
-UEFHVM01 = Class(UEFHVM01Projectile) {
-}
-
-TypeClass = UEFHVM01
-
---
+++ BlackOpsLOUD-EXUnits/mod_info.lua
@@ -0,0 +1,18 @@
+name = "BlackOps LOUD: EXUnits"
+uid = "9e8ee941-c406-4751-b367-e10000019000"
+version = 19
+copyright = "2009 Exavier Macbeth"
+description = "The ultimate unit pack, re-engineered for LOUD."
+author = "Exavier Macbeth, lt_hawkeye, orangeknight"
+icon = "/mods/BlackOpsLOUD-EXUnits/icon.png"
+selectable = true
+enabled = true
+exclusive = false
+ui_only = false
+special_thanks = "IceDreamer, Uveso, Softles, Nojoke185, ZephyrWarrior, Raistlfiren, Nocaps, Netjaxx"
+requires = {}
+requiresNames = {}
+conflicts = {
+}
+before = {}
+after = {}
